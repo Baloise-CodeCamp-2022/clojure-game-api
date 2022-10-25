fef04e1983f483374dfbca5b637b7ab06faf7877
Peter Kullmann <peter.kullmann@baloise.ch>
2022-10-25 09:23:14 +0200

    Allow POST requests (disable csrf protection)

--- src/clojure_game_api/core.clj
+++ src/clojure_game_api/core.clj
@@ -1,12 +1,10 @@
 (ns clojure-game-api.core
-  (:require [org.httpkit.server :as server]
+  (:require [clojure.data.json :as json]
+            [clojure.set :as set]
             [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer :all]
-            [clojure.pprint :as pp]
-            [clojure.string :as str]
-            [clojure.set :as set]
-            [clojure.data.json :as json])
+            [org.httpkit.server :as server]
+            [ring.middleware.defaults :refer :all])
   (:gen-class))
 
 ; ------------------- TicTacToe --------------------------------
@@ -20,7 +18,7 @@
 (defn validateBalance [board]
   (if (= board initalBoard)
     true
-    (let [freq            (frequencies (vals board))
+    (let [freq (frequencies (vals board))
           numberOfOFields (get freq :O 0)
           numberOfXFields (get freq :X 0)]
       (< (abs (- numberOfOFields numberOfXFields)) 2))))
@@ -50,17 +48,24 @@
    :headers {"Content-Type" "text/json"}
    :body    (str (json/write-str @tictactoe-board))})
 
+(defn handle-new-move [req]
+  {:status  200
+   :headers {"Content-Type" "text/json"}
+   :body    (str (json/write-str @tictactoe-board))})
+
+
 ; ------------------- App --------------------------------
 (defroutes app-routes
-  (GET "/tictactoe" [] tictactoe-handler)
-  (route/not-found "Error, page not found!"))
+           (GET "/tictactoe" [] tictactoe-handler)
+           (POST "/tictactoe/move" {body :body} (handle-new-move body))
+           (route/not-found "Error, page not found!"))
 
 (defn -main
   "This is our main entry point"
   [& args]
   (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
     ; Run the server with Ring.defaults middleware
-    (server/run-server (wrap-defaults #'app-routes site-defaults) {:port port})
+    (server/run-server (wrap-defaults #'app-routes api-defaults) {:port port})
     ; Run the server without ring defaults
     ;(server/run-server #'app-routes {:port port})
     (println (str "Running webserver at http:/127.0.0.1:" port "/"))))
