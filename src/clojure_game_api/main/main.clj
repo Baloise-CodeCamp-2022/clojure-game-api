aa516abae0c0d7bcd14a6d19fc64bfaa6cb737db
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2022-10-26 15:56:25 +0200

    starting to integrate sudoku

--- src/clojure_game_api/main/main.clj
+++ src/clojure_game_api/main/main.clj
@@ -4,13 +4,31 @@
     [clojure-game-api.main.mainweb :refer :all]
     [clojure-game-api.tictactoe.main :refer :all]
     [clojure-game-api.tictactoe.web :refer :all]
+    [clojure-game-api.sudoku.web :refer :all]
+    [compojure.core :refer :all]
+    [compojure.route :as route]
     )
   (:gen-class))
+
+(def app-routes
+  (routes
+    (GET "/tictactoe" [] handle-get-tictactoe)
+    (POST "/tictactoe/move" [] handle-new-move-json)
+    (POST "/tictactoe/game/:name" [] handle-save-json)
+    (GET "/tictactoe/game/:name" [] handle-load-json)
+    (GET "/sudoku/:id" [id] (str "<h1>Hello user " id "</h1>"))
+    (route/not-found "Error, page not found!"))
+  )
+
+
+;(def app-routes
+;  (apply routes (concat ttt-handlers sudoku-handlers)))
 
 (defn -main
   [& args]
 
   (di-reset tictactoe-di-context)
   ;(tictactoe args)
+  ;(start-web-server #'app-routes)
   (start-web-server #'app-routes)
   )
