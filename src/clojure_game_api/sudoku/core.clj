736dcad797951197881d3ac9b74641cd1677966d
Renat Sakenov <renat.sakenov@baloise.com>
2022-10-26 16:44:36 +0200

    add simple validator function

--- src/clojure_game_api/sudoku/core.clj
+++ src/clojure_game_api/sudoku/core.clj
@@ -3,5 +3,11 @@
 (def emptyBoard
   (vec (repeat 9 (vec (repeat 9 0)))))
 
+(defn validateBoard [board]
+  (let [erroneousNumbers (filter #(or (> % 9) (< % 0)) (flatten board))]
+    (if (not (empty? erroneousNumbers))
+      {:error true}
+    )))
+
 (defn setNumber [board x y n]
   (assoc board y (assoc (get board y) x n)))
--- test/clojure_game_api/sudoku/core_test.clj
+++ test/clojure_game_api/sudoku/core_test.clj
@@ -15,6 +15,46 @@
                   [0 0 0 0 0 0 0 0 0]
                   [0 0 0 0 0 0 0 0 0]
                   [0 0 0 0 0 0 0 0 0]])
-   (testing "setNumber"
+   (testing "setting a number"
             (is (= expected (setNumber emptyBoard 3 2 2 )))))
 
+(deftest validateBoard_HappyFlow
+         (def board [[0 9 0 0 0 0 0 0 0]
+                        [0 0 0 0 0 0 0 0 0]
+                        [0 0 0 2 0 0 0 0 0]
+                        [0 0 0 0 0 0 0 0 0]
+                        [0 0 0 0 0 0 0 0 0]
+                        [0 0 3 0 0 0 0 0 0]
+                        [0 0 0 0 0 0 0 0 0]
+                        [0 4 0 0 0 0 0 0 0]
+                        [0 0 0 0 0 0 0 0 0]])
+         (testing "validating a valid board"
+                  (is (= nil (validateBoard board)))))
+
+(deftest validateBoard_error_NumberTooLarge
+         (def expected {:error true})
+         (def board [[0 9 0 0 0 0 0 0 0]
+                     [0 0 0 0 0 0 0 0 0]
+                     [0 0 0 12 0 0 0 0 0]
+                     [0 0 0 0 0 0 0 0 0]
+                     [0 0 0 0 0 0 0 0 0]
+                     [0 0 3 0 0 0 0 0 0]
+                     [0 0 0 0 0 0 0 0 0]
+                     [0 4 0 0 0 0 0 0 0]
+                     [0 0 0 0 0 0 0 0 0]])
+         (testing "validating a valid board"
+                  (is (= expected (validateBoard board)))))
+
+(deftest validateBoard_error_NumberTooSmall
+         (def expected {:error true})
+         (def board [[0 9 0 0 0 0 0 0 0]
+                     [0 0 0 0 0 0 0 0 0]
+                     [0 0 0 0 0 0 0 0 0]
+                     [0 0 0 0 -1 0 0 0 0]
+                     [0 0 0 0 0 0 0 0 0]
+                     [0 0 3 0 0 0 0 0 0]
+                     [0 0 0 0 0 0 0 0 0]
+                     [0 4 0 0 0 0 0 0 0]
+                     [0 0 0 0 0 0 0 0 0]])
+         (testing "validating a valid board"
+                  (is (= expected (validateBoard board)))))
