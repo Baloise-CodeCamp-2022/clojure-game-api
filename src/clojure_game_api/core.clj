ea22dab04885779a8d3c9b572815821523d4a0a4
Peter Kullmann <peter.kullmann@baloise.ch>
2022-10-26 10:20:02 +0200

    A slightly better opponent

--- src/clojure_game_api/core.clj
+++ src/clojure_game_api/core.clj
@@ -99,6 +99,42 @@
   )
 )
 
+; CPU player 2 - a bit more strategy
+; Get all fields of the given field in its row
+(defn tttRow [field]
+  (let [letter (subs (name field) 0 1)]
+    (set (map (fn [x] (keyword (str letter x))) '(1, 2, 3)))
+    ))
+(defn tttCol [field]
+  (let [tttNum (subs (name field) 1)]
+    (set (map (fn [x] (keyword (str x tttNum))) '("A", "B", "C")))
+    ))
+; how many tics does player have in the given fields
+(defn ticCount [board fields player]
+  (let [partOfBoard (select-keys board fields)]
+    (count (filter (fn [e] (= (second e) player)) partOfBoard))))
+; move gets a value of 10 if it prohibits a win of the opponent and 0 otherwise
+(defn valueOfMove [board move player]
+  ; tttRow board :A2 --> :A1 :A2 :A3
+  ; tttCol
+  ; tttDiag1 tttDiag2
+  (let [other (if (= player :X) :O :X)
+        rowTics (ticCount board (tttRow move) other)
+        colTics (ticCount board (tttCol move) other)
+        ; todo myDiag1/2
+        ]
+    (if (or (= rowTics 2) (= colTics 2)) 10 0)
+    ))
+
+(defn cpuOpponent2 [board player]
+  (let [validMoves (set/difference validCoordinates (into #{} (keys board)))
+        movesWithWeight (into {} (for [m validMoves] [m (valueOfMove board m player)]))
+        targetField (first (last (sort-by second movesWithWeight)))]
+    (makeMove board targetField player)
+    ))
+
+
+
 (defn saveBoard [boardName board]
   (swap! boardRepository assoc (keyword boardName) board))
 
